5ca05fc52be009381453c297994ebe91dc646646
novolunt <novolunt@oc.com>
2025-06-19 11:30:46 +0800

    'change'

--- pkg/gostpkg/gost.go
+++ pkg/gostpkg/gost.go
@@ -5,32 +5,29 @@ import (
 	"log"
 
 	"github.com/go-gost/core/chain"
-	"github.com/go-gost/core/hop"
 	socks5c "github.com/go-gost/x/connector/socks/v5"
 	socks5h "github.com/go-gost/x/handler/socks/v5"
 	"github.com/go-gost/x/listener/tcp"
 )
 
-// StartSocks5Chain 启动 socks5+chain 代理服务
+// StartSocks5Chain 启动 socks5+chain 代理服务（go-gost v3 正确用法）
 func StartSocks5Chain(listenAddr, remoteAddr, username, password string) error {
-	// 创建 socks5 v5 connector
-	conn := socks5c.NewConnector(
-		socks5c.AuthOption(username, password),
-	)
+	// 创建 socks5 connector
+	conn := socks5c.NewConnector()
 
 	// 组装 chain
-	n := chain.NewNode(remoteAddr, chain.WithConnector(conn))
-	h := hop.NewHop(hop.WithNodes(n))
-	c := chain.NewChain(chain.WithHops(h))
+	n := chain.NewNode(remoteAddr, chain.WithAuth(username, password), chain.WithConnector(conn))
+	h := chain.NewHop(n)
+	c := chain.NewChain(h)
 
 	// 创建 socks5 handler（本地认证）
 	hdl := socks5h.NewHandler(
-		socks5h.AuthOption(username, password),
-		socks5h.ChainOption(c),
+		socks5h.WithAuth(username, password),
+		socks5h.WithChain(c),
 	)
 
 	// 创建 TCP listener
-	ln, err := tcp.NewListener(listenAddr)
+	ln, err := tcp.NewListener(context.Background(), listenAddr)
 	if err != nil {
 		return err
 	}
