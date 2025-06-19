8c2dc57d0a0a013965f9e0e18270f72f110db76f
novolunt <novolunt@oc.com>
2025-06-19 11:45:12 +0800

    'change'

--- pkg/gostpkg/gost.go
+++ pkg/gostpkg/gost.go
@@ -3,8 +3,11 @@ package gostpkg
 import (
 	"context"
 	"log"
+	"net"
+	"net/url"
 
 	"github.com/go-gost/core/chain"
+	"github.com/go-gost/core/handler"
 	socks5c "github.com/go-gost/x/connector/socks/v5"
 	socks5h "github.com/go-gost/x/handler/socks/v5"
 	"github.com/go-gost/x/listener/tcp"
@@ -16,31 +19,17 @@ func StartSocks5Chain(listenAddr, remoteAddr, username, password string) error {
 	conn := socks5c.NewConnector()
 
 	// 组装 node
-	n := &chain.Node{
-		Addr:      remoteAddr,
-		Connector: conn,
-		Auth: &chain.Auth{
-			Username: username,
-			Password: password,
-		},
-	}
+	n := chain.NewNode("proxy-node", remoteAddr)
 	// 组装 hop
-	h := &chain.Hop{
-		Nodes: []*chain.Node{n},
-	}
+	h := &chain.Hop{Nodes: []*chain.Node{n}}
 	// 组装 chain
-	c := &chain.Chain{
-		Hops: []*chain.Hop{h},
-	}
+	c := &chain.Chain{Hops: []*chain.Hop{h}}
 
 	// 创建 socks5 handler
-	hdl := socks5h.NewHandler(&socks5h.Config{
-		Auth: &socks5h.Auth{
-			Username: username,
-			Password: password,
-		},
-		Chain: c,
-	})
+	hdl := socks5h.NewHandler(
+		handler.AuthOption(url.UserPassword(username, password)),
+		handler.RouterOption(c),
+	)
 
 	// 创建 TCP listener
 	ln, err := tcp.NewListener(listenAddr)
@@ -50,8 +39,18 @@ func StartSocks5Chain(listenAddr, remoteAddr, username, password string) error {
 
 	// 启动服务
 	go func() {
-		if err := hdl.Serve(ln); err != nil {
-			log.Fatalf("socks5 服务启动失败: %v", err)
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				log.Printf("accept error: %v", err)
+				continue
+			}
+			go func(c net.Conn) {
+				defer c.Close()
+				if err := hdl.Handle(context.Background(), c); err != nil {
+					log.Printf("handle error: %v", err)
+				}
+			}(conn)
 		}
 	}()
 	return nil
